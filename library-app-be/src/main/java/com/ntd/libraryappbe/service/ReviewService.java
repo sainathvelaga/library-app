7455b15a5670ff0c940169febccc0147147d6e4d
Sainath <sainath.velaga@gmail.com>
2025-12-27 12:42:21 +0530

    backend

--- library-app-be/src/main/java/com/ntd/libraryappbe/service/ReviewService.java
+++ library-app-be/src/main/java/com/ntd/libraryappbe/service/ReviewService.java
@@ -1,51 +1,40 @@
 package com.ntd.libraryappbe.service;
 
-import com.ntd.libraryappbe.dao.BookRepository;
-import com.ntd.libraryappbe.dao.ReviewRepository;
 import com.ntd.libraryappbe.entity.Review;
+import com.ntd.libraryappbe.repository.ReviewRepository;
 import com.ntd.libraryappbe.requestmodels.ReviewRequest;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.Date;
-import java.time.LocalDate;
 
 @Service
-@Transactional
 public class ReviewService {
-    private ReviewRepository reviewRepository;
 
-    @Autowired
+    private final ReviewRepository reviewRepository;
+
     public ReviewService(ReviewRepository reviewRepository) {
         this.reviewRepository = reviewRepository;
     }
 
-    public void postReview(String userEmail, ReviewRequest reviewRequest) throws Exception {
-        Review validateReview = reviewRepository.findByUserEmailAndAndBookId(userEmail, reviewRequest.getBookId());
-        if (validateReview != null) {
-            throw new Exception("You have already reviewed this book");
-        }
-
-        Review review = new Review();
-        review.setBookId(reviewRequest.getBookId());
-        review.setRating(reviewRequest.getRating());
-        review.setUserEmail(userEmail);
-        if (reviewRequest.getReviewDescription().isPresent()) {
-            review.setReviewDescription(reviewRequest.getReviewDescription().map(
-                    Object::toString).orElse(null)
-            );
-        }
-        review.setDate(Date.valueOf(LocalDate.now()));
-        reviewRepository.save(review);
+    public boolean userReviewListed(String userEmail, Long bookId) {
+        return reviewRepository.findByUserEmailAndBookId(userEmail, bookId).isPresent();
     }
 
-    public Boolean userReviewListed(String userEmail, Long bookId) {
-        Review validateReview = reviewRepository.findByUserEmailAndAndBookId(userEmail, bookId);
-        if (validateReview != null) {
-            return true;
-        }else {
-            return false;
+    public void postReview(String userEmail, ReviewRequest reviewRequest) {
+
+        Review review = new Review();
+
+        review.setUserEmail(userEmail);
+        review.setBookId(reviewRequest.getBookId());
+        review.setRating(reviewRequest.getRating());
+
+        String description = reviewRequest.getReviewDescription();
+        if (description != null && !description.isEmpty()) {
+            review.setReviewDescription(description);
         }
+
+        review.setDate(new Date(System.currentTimeMillis()));
+
+        reviewRepository.save(review);
     }
 }
